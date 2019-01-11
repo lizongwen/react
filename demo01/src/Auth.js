7b022f1efdc4363f8e47596c373e1b5f03cc3fc8
lizongwen <93700983@qq.com>
2019-01-11 14:57:37 +0800

    enter

--- demo01/src/Auth.js
+++ demo01/src/Auth.js
@@ -3,10 +3,16 @@ import { connect } from 'react-redux';
 import { login, logout } from './Auth.redux'
 import { Redirect } from 'react-router-dom';
 import { Button } from 'antd-mobile';
+import AuthStyle from './Auth.css'
 const mapStatetoProps = (state) => {
 	return state.auth
 }
 const actionCreators = { login, logout };
+let styles = {
+	tip: {
+		fontWeight: 700
+	}
+}
 
 class Auth extends Component {
 	// constructor(props) {
@@ -14,9 +20,10 @@ class Auth extends Component {
 	// }
 	render() {
 		return (
-			<div>
+			//  className={AuthStyle.auth}写法是CSSmodule写法
+			<div style={{ fontSize: 18 }}>
 				{this.props.isAuth ? <Redirect to='/dashboard' /> : null}
-				<h2>没有权限，需要登录</h2>
+				<h2 className="tip" style={styles.tip} >没有权限，需要登录</h2>
 				<Button type="primary" onClick={this.props.login}>登录</Button>
 			</div>
 		)
